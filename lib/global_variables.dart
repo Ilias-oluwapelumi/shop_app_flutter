92ea8487461870a28402827b38fb35b9ba570107
Oyedokun Ilias <oyedokunilias@gmail.com>
2024-05-16 15:26:50 +0200

    first commit - comitting all files to the repository

---
+++ lib/global_variables.dart
@@ -0,0 +1,45 @@
+final products = [
+  {
+    'id':'0',
+    'title':'Men\'s Nike Shoes',
+    'price': 44.52,
+    'imageUrl': 'assests/images/images-1.png.png',
+    'company': 'Nike',
+    'sizes': [9,10,11,12],
+
+
+  },
+   {
+    'id':'1',
+    'title':'Addidas',
+    'price': 44.52,
+    'imageUrl': 'assests/images/images-2.png.png',
+    'company': 'Nike',
+    'sizes': [9,10,11,12],
+
+
+  },
+   {
+    'id':'2',
+    'title':'Men\'s Nike Shoes',
+    'price': 44.52,
+    'imageUrl': 'assests/images/images-3.png.png',
+    'company': 'Nike',
+    'sizes': [9,10,11,12],
+
+
+  },
+   {
+  'id':'3',
+    'title':'Gucci',
+    'price': 44.52,
+    'imageUrl': 'assests/images/images-4.png.png',
+    'company': 'Beta',
+    'sizes': [9,10,11,12],
+
+
+  },
+];
+  
+
+  
